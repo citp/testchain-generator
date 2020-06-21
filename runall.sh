93c42f0b71b822d7729d27abae22477a00b16eff
John L. Jegutanis <erasmospunk@gmail.com>
2020-06-22 00:15:10 +0300

    Fix travis tests

--- runall.sh
+++ runall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 generate_chain.py --output-dir=output
+python3 generate_chain.py --output-dir=output --chain=btc --exec=./bin/bitcoind
 python3 generate_chain.py --output-dir=output --chain=bch --exec=./bin/bitcoin-cash
 python3 generate_chain.py --output-dir=output --chain=ltc --exec=./bin/litecoind
